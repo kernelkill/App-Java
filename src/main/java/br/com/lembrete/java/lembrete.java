4fe9416dcbb26887ce5de191a1273a6286a7e028
Kachorroski <kernelkill@outlook.com>
2017-07-31 21:43:40 -0400

    Criando a classe lembrete e mapeando.

---
+++ src/main/java/br/com/lembrete/java/lembrete.java
@@ -0,0 +1,56 @@
+package br.com.lembrete.java;
+
+
+import javax.persistence.*;
+
+@Entity
+public class lembrete {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    public lembrete() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "lembrete{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
