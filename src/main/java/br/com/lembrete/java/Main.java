0399f84e5403c68079f5fa76535afc881ad26560
Kachorroski <kernelkill@outlook.com>
2017-07-31 23:01:25 -0400

    Inserindo dados no banco com o EntityManager

--- src/main/java/br/com/lembrete/java/Main.java
+++ src/main/java/br/com/lembrete/java/Main.java
@@ -1,5 +1,6 @@
 package br.com.lembrete.java;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -12,5 +13,22 @@ public class Main {
         entityManagerFactory = Persistence.createEntityManagerFactory("hibernatejpa");
 
 
+        Lembrete lembrete = new Lembrete();
+        lembrete.setTitulo("Comprar Leite");
+        lembrete.setDescricao("Comprar o leite hoje as 10:30H");
+
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            em.persist(lembrete);
+            em.getTransaction().commit();
+        }catch (Exception e){
+            em.getTransaction().rollback();
+
+            System.out.println("INSERT: " + e.getMessage());
+        }finally {
+            em.close();
+        }
     }
 }
